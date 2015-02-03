5a5b46528b09af76f11d2e2383b5ca636aa2efa8
OHTA Shogo <athos0220@gmail.com>
2015-02-03 15:33:50 +0900

    Implement author pages

--- src/clojournal/models/author.clj
+++ src/clojournal/models/author.clj
@@ -10,15 +10,18 @@
   (-> (util/fix-object author)
       (dissoc :password)))
 
+(defn find-author [id]
+  (fix-author (mc/find-one-as-map db "authors" {:_id id})))
+
 (defn register-author! [& {:keys [name email password]}]
   (assert (and (seq name) (seq email) (seq password)))
   (let [password (encrypt password)
         author {:_id email :name name
                 :created-at (java.util.Date.) :password password}]
-    (-> (mc/insert-and-return db "author" author)
+    (-> (mc/insert-and-return db "authors" author)
         fix-author)))
 
 (defn authenticate [email password]
-  (let [{crypted-pass :password :as author}  (mc/find-one-as-map db "author" {:_id email})]
+  (let [{crypted-pass :password :as author}  (mc/find-one-as-map db "authors" {:_id email})]
     (when (check password crypted-pass)
       (fix-author author))))
--- src/clojournal/routes/author.clj
+++ src/clojournal/routes/author.clj
@@ -3,6 +3,7 @@
             [clojournal.layout :as layout]
             [clojournal.util :as util]
             [clojournal.models.author :as author]
+            [clojournal.models.article :as article]
             [noir.session :as session]
             [noir.response :as response]))
 
@@ -12,18 +13,28 @@
 
 (defn top-page []
   (layout/render
-    "home.html" {:content (util/md->html "/md/docs.md")}))
+    "author/home.html" {:articles (article/latest-articles 10)}))
+
+(defn post-article [author-id title content tags]
+  (let [tags' (clojure.string/split tags #"\s*,\s*")]
+    (article/add-article! :title title :content content :author author-id :tags tags')
+    (response/redirect "/")))
 
 (defn wrap-auth [app]
   (fn [req]
-    (if (session/get :author)
-      (app req)
+    (if-let [id (session/get :author)]
+      (let [author (author/find-author id)
+            req (assoc req :author author)]
+        (app req))
       (response/redirect "/"))))
 
 (def author-routes*
   (-> (routes
         (GET "/" []
-             (top-page)))
+             (top-page))
+        (POST "/article" {{:keys [title content tags]} :params}
+              (let [author-id (session/get :author)]
+                (post-article author-id title content tags))))
       wrap-auth))
 
 (defn authenticate [email password]
@@ -37,5 +48,5 @@
        (login-page))
   (POST "/login" {{:keys [email password]} :params}
         (authenticate email password))
-  (context "/author" req
-           (author-routes* req)))
+  (context "/author" _
+           author-routes*))
