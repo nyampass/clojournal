03653cc736e4567cdc1e36c3820a0b8e5cf549dc
OHTA Shogo <athos0220@gmail.com>
2015-02-12 14:51:22 +0900

    Implement latest articles section in sidebar

--- src/clojournal/layout.clj
+++ src/clojournal/layout.clj
@@ -3,7 +3,8 @@
             [clojure.string :as s]
             [ring.util.response :refer [content-type response]]
             [compojure.response :refer [Renderable]]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clojournal.models.article :as article]))
 
 (parser/set-resource-path!  (clojure.java.io/resource "templates"))
 
@@ -20,7 +21,9 @@
                     ;; example when using mock requests), then
                     ;; .getContextPath might not exist
                     (try (.getContextPath context)
-                         (catch IllegalArgumentException _ context))))
+                         (catch IllegalArgumentException _ context)))
+                  ;; FIXME: the following line always issues db query even if not necessary
+                  :latest-articles (:articles (article/latest-articles 0 10)))
         (parser/render-file (str template))
         response)
       "text/html; charset=utf-8")))
