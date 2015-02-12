3ec1c01337dd7a4777cefbe0b40a40ad21051f23
OHTA Shogo <athos0220@gmail.com>
2015-02-12 13:11:15 +0900

    Add article preprocess for syntax highlighting

--- src/clojournal/routes/article.clj
+++ src/clojournal/routes/article.clj
@@ -1,8 +1,31 @@
 (ns clojournal.routes.article
   (:require [compojure.core :refer :all]
+            [clojure.string :as str]
             [clojournal.layout :as layout]
             [clojournal.models.article :as article]
             [clojournal.models.author :as author]))
+
+(defn parse-content [content]
+  (lazy-seq
+    (let [[_ pre code post] (re-matches #"(?s)(?:(.*?)(?:```(.*?)```))?(.*)" content)]
+      (cond (= post "") nil
+            (nil? code) [[post nil]]
+            :else (cons [pre code] (parse-content post))))))
+
+(defn preprocess-content [content]
+  (let [content (-> content
+                    (str/replace "<div>" "")
+                    (str/replace "</div>" "\n"))]
+    (->> (for [[text code] (parse-content content)
+               :let [text (-> text
+                              (str/replace "\n" "")
+                              (str/replace "<br>" "</p>\n<p>"))]]
+           (if code
+             (format "%s</p>\n<pre class=\"brush: clojure\">%s</pre>\n<p>" text code)
+             text))
+         str/join
+         (format "<p>%s</p>")
+         ((fn [s] (str/replace s "<p></p>" ""))))))
 
 (defn article-page [id]
   (when-let [article (article/find-article id)]
@@ -10,7 +33,7 @@
       (layout/render
         "article.html"
         {:title (:title article)
-         :content (:content article)
+         :content (preprocess-content (:content article))
          :author (:name author)
          :tags (clojure.string/join ", " (:tags article))
          :updated-at (:updated-at article)}))))
