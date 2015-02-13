ce647ec616c1fbb93fec90dde01070f27dc9de46
OHTA Shogo <athos0220@gmail.com>
2015-02-13 10:21:55 +0900

    Remove index from article content, which caused some errors

--- src/clojournal/db.clj
+++ src/clojournal/db.clj
@@ -10,6 +10,5 @@
 
 (defn create-indexes []
   (mc/ensure-index db "articles" (array-map :title 1) {:name "by-title"})
-  (mc/ensure-index db "articles" (array-map :content 1) {:name "by-content"})
   (mc/ensure-index db "articles" (array-map :author 1) {:name "by-author"})
   (mc/ensure-index db "articles" (array-map :tags 1) {:name "by-tags"}))
