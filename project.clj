a770774912b8adbc4162f5de7ba4b0e62c9dcdac
HARUYAMA Seigo <haruyama@unixuser.org>
2015-02-24 13:57:25 +0900

    Add :eastwood {:exclude-linters [:constant-test]}
    https://github.com/jonase/eastwood#constant-test

--- project.clj
+++ project.clj
@@ -42,4 +42,5 @@
                       (pjstadig.humane-test-output/activate!)]
          :env {:dev true
                :mongo-url "mongodb://localhost/clojournal"}}}
+  :eastwood  {:exclude-linters  [:constant-test]}
   :min-lein-version "2.0.0")
