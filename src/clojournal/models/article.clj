34678d63ee76ef023565e9ce02f1eb7eed4c2ac1
OHTA Shogo <athos0220@gmail.com>
2015-03-06 18:30:43 +0900

    Fix AND search

--- src/clojournal/models/article.clj
+++ src/clojournal/models/article.clj
@@ -29,18 +29,17 @@
      :older-page (when (< (* (inc page) per-page) num) (inc page))}))
 
 (defn search-articles [words page per-page]
-  (let [re (->> words
-                (map #(str "(?:" (Pattern/quote %) ")"))
-                (str/join "|")
-                (str "(?i)")
-                re-pattern)
+  (let [condition (->> (for [word words
+                             :let [p (re-pattern (str "(?i)" (Pattern/quote word)))]]
+                         {mo/$or [{:content p} {:title p} {:author p}]})
+                       (array-map mo/$and))
         skip (* page per-page)
         articles (->> (mq/with-collection db "articles"
-                          (mq/find {mo/$or [{:content re} {:title re} {:author re}]})
+                          (mq/find condition)
                           (mq/skip skip)
                           (mq/limit per-page))
                       (map fix-article))
-        num (mc/count db "articles" {mo/$or [{:content re} {:title re} {:author re}]})]
+        num (mc/count db "articles" condition)]
     {:articles articles
      :total num
      :first (+ skip 1)
